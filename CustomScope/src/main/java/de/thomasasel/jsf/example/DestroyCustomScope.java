8d3b1d610bfeb8fc69edcf466fb495e31f771c8a
tasel <thomas.asel@oio.de>
2012-07-19 11:41:01 +0200

    New example: CustomScope

---
+++ CustomScope/src/main/java/de/thomasasel/jsf/example/DestroyCustomScope.java
@@ -0,0 +1,25 @@
+package de.thomasasel.jsf.example;
+
+import java.util.Map;
+
+import javax.faces.context.FacesContext;
+import javax.faces.event.AbortProcessingException;
+import javax.faces.event.ActionEvent;
+import javax.faces.event.ActionListener;
+
+public class DestroyCustomScope implements ActionListener {
+
+	@Override
+	public void processAction(final ActionEvent event) throws AbortProcessingException {
+
+		FacesContext facesContext = FacesContext.getCurrentInstance();
+		Map<String, Object> sessionMap = facesContext.getExternalContext().getSessionMap();
+
+		CustomScope customScope = (CustomScope) sessionMap.get(CustomScope.SCOPE_NAME);
+		customScope.notifyDestroy(facesContext);
+
+		sessionMap.remove(CustomScope.SCOPE_NAME);
+
+	}
+
+}
